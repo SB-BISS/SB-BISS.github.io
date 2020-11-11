ffa7ca9389d38f982da8bdb5425e68e99d6c4976
maltus <maltus>
2020-11-11 22:27:17 +0100

    page substrate Kusama

---
+++ myBlockInfo/build/precache-manifest.4c283ae452887e1a111369397d2ef875.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "639bc842979661abe0f9284a6aa7c087",
+    "url": "/myBlockInfo/index.html"
+  },
+  {
+    "revision": "1c6232b6b6bc00a0a2a6",
+    "url": "/myBlockInfo/static/css/2.3fdc8e27.chunk.css"
+  },
+  {
+    "revision": "1c6232b6b6bc00a0a2a6",
+    "url": "/myBlockInfo/static/js/2.8ec59a7c.chunk.js"
+  },
+  {
+    "revision": "f9baea58926422c9ca74d98ae42cdbab",
+    "url": "/myBlockInfo/static/js/2.8ec59a7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21b92c39bf8ed1b6fad3",
+    "url": "/myBlockInfo/static/js/main.b94892fd.chunk.js"
+  },
+  {
+    "revision": "50e17f30b9b6997e9ab4",
+    "url": "/myBlockInfo/static/js/runtime-main.2daeb7dd.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/myBlockInfo/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/myBlockInfo/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/myBlockInfo/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/myBlockInfo/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/myBlockInfo/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/myBlockInfo/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/myBlockInfo/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/myBlockInfo/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/myBlockInfo/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/myBlockInfo/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/myBlockInfo/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/myBlockInfo/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/myBlockInfo/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/myBlockInfo/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/myBlockInfo/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/myBlockInfo/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
